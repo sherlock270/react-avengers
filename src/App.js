5da231b765755a65d63433fea0594e57d835dd86
Charles Cortinas <crcortinas@gmail.com>
2018-07-30 12:07:16 -0500

    Avengers
    Listed Avengers

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {Avengers: ['Black Widow', 'Falcon', 'Iron Man', 'The Incredible Hulk']};
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Avengers: by Charles Cortinas</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.Avengers.map(avenger => {return <div>{avenger}</div>})}
       </div>
     );
   }
